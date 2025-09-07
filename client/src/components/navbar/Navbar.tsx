db60054f58666147943001f2844b5f23db74a6d5
RakshitGumber <gumberrakshit@gmail.com>
2025-09-07 17:01:08 +0530

    added sidebar expand button in navbar

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,14 +1,18 @@
 import "./navbar.css";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="navbar-content">
-        <h1 className="title">SmartMoney</h1>
-        <div></div>
+        <div className="title">
+          <button className="sidebar-toggle">
+            <Icon icon="streamline-ultimate:money-wallet-open" />
+          </button>
+          <h1>SmartMoney</h1>
+        </div>
       </div>
     </div>
   );
 };
 export default Navbar;
-    
